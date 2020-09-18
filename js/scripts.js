ef033b0a521e45264835729e8d8a05f73192bb48
Seanlebonne <sean.chen0818@gmail.com>
2020-09-18 00:57:53 -0400

    most components are finished; may be ready for deployment of alpha version

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ $(function () {
     });
 });
 
+document.getElementById("emailCopy").onclick = function(e){
+    let email = "sean.chen0818@gmail.com";
+    copyStringToClipboard(email);
+}
+
+function copyStringToClipboard (str) {
+    
+    var el = document.createElement('textarea');
+    
+    el.value = str;
+    
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    
+    el.select();
+    
+    document.execCommand('copy');
+    
+    document.body.removeChild(el);
+ }
+ 
